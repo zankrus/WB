c0b004cd11f925f25764c75c5a0102228e830114
Роберт Ислямов <IslyamovRR@moffice.akbars.ru>
2024-05-03 22:05:38 +0300

    Закончил разминку

--- test_simple.py
+++ test_simple.py
@@ -14,7 +14,7 @@ import pytest
 )
 def test_get_positive(client_non_auth, userName, statusCode, expectedResult):
     """
-    Позитивный тест на получение юзера. С параметризацией через @pytest.mark.parametrize
+    Позитивный тест на получение юзера. С параметризацией через @pytest.mark.parametrize и явно через pytest.param
     """
     res = client_non_auth.get_user(userName)
     assert res.status_code == statusCode
@@ -46,6 +46,7 @@ def test_get_negative(client_non_auth, userName, statusCode, expectedError):
     # по желанию проверка хедеров и кук
 
 
+## Пример явной параметризации фикстуры
 def test_get_positive_with_param_fixture(clientParametrized):
     """
     Пример параметризации через параметризованную фикстуру clientParametrized
@@ -55,37 +56,132 @@ def test_get_positive_with_param_fixture(clientParametrized):
     res = cl.get_user("theUser")
     assert res.status_code == 200
     assert res.json() == {'id': 10, 'username': 'theUser', 'firstName': 'John',
-                             'lastName': "James' and 1 = if(5=5, sleep(2), 0) and '1'='1", 'email': 'john@email.com',
-                             'password': '12345', 'phone': '12345', 'userStatus': 1}
+                          'lastName': "James' and 1 = if(5=5, sleep(2), 0) and '1'='1", 'email': 'john@email.com',
+                          'password': '12345', 'phone': '12345', 'userStatus': 1}
     # по желанию проверка хедеров и кук
 
 
-@pytest.mark.parametrize( "user,password",[ ("some","some_pass") ] )
-def test_get_positive_with_implicit_param_fixture(client_with_implicit_auth,user,password):
+## Пример НЕявной параметризации фикстуры
+@pytest.mark.parametrize("user,password", [("some", "some_pass")])
+def test_get_positive_with_implicit_param_fixture(client_with_implicit_auth, user, password):
     """
     Пример параметризации через параметризованную фикстуру clientParametrized с неявной передачей.
-    В чем соль - фикстура clientParametrized ожидает 2 фикстуры на вход user, password . Но они не существуют. Они будут создаваться
+    В чем соль - фикстура client_with_implicit_auth ожидает 2 фикстуры на вход user, password . Но они не существуют. Они будут создаваться
     в процессе выполнения
     """
     cl = client_with_implicit_auth
     res = cl.get_user("theUser")
     assert res.status_code == 200
     assert res.json() == {'id': 10, 'username': 'theUser', 'firstName': 'John',
-                             'lastName': "James' and 1 = if(5=5, sleep(2), 0) and '1'='1", 'email': 'john@email.com',
-                             'password': '12345', 'phone': '12345', 'userStatus': 1}
+                          'lastName': "James' and 1 = if(5=5, sleep(2), 0) and '1'='1", 'email': 'john@email.com',
+                          'password': '12345', 'phone': '12345', 'userStatus': 1}
 
 
+def test_post_create_user_positive(client_non_auth):
+    """
+    Позитивный сценарий на создание юзера и проверка совпадения данных через ручку получения юзера
+    """
+
+    test_data = {
+        "id": 14430,
+        "username": "theUser",
+        "firstName": "John",
+        "lastName": "James",
+        "email": "john@email.com",
+        "password": "12345",
+        "phone": "12345",
+        "userStatus": 1
+    }
+    res = client_non_auth.post_create_user(data=test_data)
+    assert res.status_code == 200
+    assert res.json() == test_data
+
+    getUserResult = client_non_auth.get_user("theUser")
+    assert res.json() == getUserResult.json()
 
 
+def test_post_create_user_positive_db(client_non_auth):
+    """
+    Позитивный сценарий на создание юзера и проверка совпадения данных через запрос в БД.
+    Лучше такое в юниты вынести, чтобы не тестить клиент к базе, а реальную реализацию
+    """
+
+    test_data = {
+        "id": 14430,
+        "username": "theUser",
+        "firstName": "John",
+        "lastName": "James",
+        "email": "john@email.com",
+        "password": "12345",
+        "phone": "12345",
+        "userStatus": 1
+    }
+    res = client_non_auth.post_create_user(data=test_data)
+    assert res.status_code == 200
+    assert res.json() == test_data
+
+    ##DB client делаем запрос в базу
 
 
-def test_post():
-    assert False
+def test_post_create_user_negative(client_non_auth):
+    """
+   Используем тест-дизайн и генерируем проверки. ( Невалидные данные, уже созданные юзеры, удаленные юзеры ( при софт делете)
+   В идеале используем знания, какие юниты у нас уже есть, что неплодить дорогие проверки "маппингов" или
+   не делать проверки типов данных, если у нас жесткий контракт.
+    """
+    assert True
 
 
-def test_put():
-    assert False
+def test_put_edit_user_positive(client_non_auth):
+    """
+    Позитивный сценарий на изменение юзера и проверка совпадения данных через запрос в ручку получения юзера.
+     """
+    test_data = {
+        "id": 144300,
+        "username": "theUser",
+        "firstName": "John",
+        "lastName": "James",
+        "email": "john@email.com",
+        "password": "12345",
+        "phone": "12345",
+        "userStatus": 1
+    }
+    res = client_non_auth.post_create_user(data=test_data)
+    assert res.status_code == 200
+    assert res.json() == test_data
+
+    getUserResult = client_non_auth.get_user("theUser")
+    assert res.json() == getUserResult.json()
 
 
-def test_delete():
-    assert False
+def test_put_edit_user_positive_db(client_non_auth):
+    """
+   Позитивный сценарий на изменение юзера и проверка совпадения данных через запрос в БД. .
+    """
+    assert True
+
+
+def test_delete_positive(client_non_auth):
+    """
+       Позитивный тест на удаление юзера. Проверка через запрос в ручку получения юзера.
+       """
+    res = client_non_auth.delete_user("User1")
+    assert res.status_code == 200
+    assert res.json() == {"success": True}
+
+    getUserResult = client_non_auth.get_user("theUser")
+    assert getUserResult.status_code == 200
+    assert getUserResult.json() == {"success": True}
+
+
+def test_delete_positive_db(client_non_auth):
+    """
+       Позитивный тест на удаление юзера. Проверка через запрос к базе
+       """
+    res = client_non_auth.delete_user("User1")
+    assert res.status_code == 200
+    assert res.json() == {"success": True}
+
+    getUserResult = client_non_auth.get_user("theUser")
+    assert getUserResult.status_code == 200
+    assert getUserResult.json() == {"success": True}
